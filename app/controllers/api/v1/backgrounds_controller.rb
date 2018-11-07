40274a377f9335cc6af4f4e584553099e038d51a
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-11-07 12:42:57 -0700

    Flickr Service complete and controller returns image url

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    
+    background = Background.new(params[:location])
+    render json: background.images.sample
   end
 end
--- app/facades/background.rb
+++ app/facades/background.rb
@@ -6,7 +6,15 @@ class Background
     @city_state = city_state
   end
 
+  def images
+    background_service.image_data(coordinates)
+  end
+
   private
+
+  def coordinates
+    @coordinates ||= geocoder_service.get_lat_long(@city_state)
+  end
 
   def geocoder_service
     GoogleService.new
